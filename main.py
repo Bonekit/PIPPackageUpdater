e3e05ccc33c0e07ecb6ce63e8edf1b9132428804
Tobias Menzel <tobias.menzel@posteo.de>
2018-10-09 22:37:38 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,28 @@
+
+# Title                 : PIP Updater v1.0
+# Description           : PIP Updater v1.0
+# Author                : Tobias Menzel
+# Date                  : 09.10.2018
+# Notes                 : Only for PIP Versions >= 10.0.1
+# Python_version        : 3.7
+# ==========================================================================================
+
+# Import modules for the PIP Updater
+from subprocess import call
+import pkg_resources
+
+# PIP Updater Function.
+
+
+def main():
+    packages = [dist.project_name for dist in pkg_resources.working_set]
+    call("pip install --upgrade " + ' '.join(packages), shell=True)
+
+
+# ===============================
+#        APPLICATION START
+# ===============================
+
+
+if __name__ == "__main__":
+    main()
