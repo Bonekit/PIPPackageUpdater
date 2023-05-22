ae7aee64e927b924bdd20df6f880a1dbc79a1636
Tobias Menzel <Bonekit@users.noreply.github.com>
2023-05-22 09:00:58 +0200

    Update PIP Package Updater to v1.1
    - Replace deprecated subprocess.call() with subprocess.run()
    - Capture stdout and stderr using subprocess.PIPE
    - Use pip module instead of calling 'pip install' command
    - Improved compatibility and error handling

--- updater.py
+++ updater.py
@@ -1,30 +1,26 @@
-
-# Title                 : PIP Package Updater v1.0
+# Title                 : PIP Package Updater v1.1
 # Description           : Update all installed pip-packages
 # Author                : Tobias Menzel
 # Date                  : 09.10.2018
-# Modified              : 01.04.2021
+# Modified              : 22.05.2023
 # Notes                 : Only for PIP Versions >= 10.0.1
 # Tested:               : Python 3.7 or higher 
 # ==========================================================================================
 
 # Module dependencies
-from subprocess import call
+import subprocess
 import pkg_resources
+import pip
 
 # Main function
-
-
 def main():
-    """ Get all packages and update"""
+    """ Get all packages and update """
     packages = [dist.project_name for dist in pkg_resources.working_set]
-    call("pip install --upgrade " + ' '.join(packages), shell=True)
-
+    subprocess.run([sys.executable, '-m', 'pip', 'install', '--upgrade'] + packages, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 # ===============================
 #        APPLICATION START
 # ===============================
-
 
 if __name__ == "__main__":
     main()
